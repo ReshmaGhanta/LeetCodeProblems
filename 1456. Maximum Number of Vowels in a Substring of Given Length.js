ccebf93f04710c8ca56f099e780acdc959fa0f99
Reshma Ghanta <45803247+ReshmaGhanta@users.noreply.github.com>
2024-03-26 11:32:00 -0400

    Create 1456. Maximum Number of Vowels in a Substring of Given Length.js

---
+++ 1456. Maximum Number of Vowels in a Substring of Given Length.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var maxVowels = function(s, k) {
+const vowels=['a','e','i','o','u'];
+let max=0;
+let left=0;
+let count=0
+    for(let right=0;right<s.length;right++){
+        if(vowels.includes(s[right])){
+            count++;
+        }
+        if(right-left+1>k){
+            if(vowels.includes(s[left])){
+                count--;
+            }
+            left++
+        }
+
+        max=Math.max(max,count);
+        if(max===k) return max;
+    }
+
+    return max;
+};
+
