b4c287009e8a2179313ebd1188265028c156e43f
Reshma Ghanta <45803247+ReshmaGhanta@users.noreply.github.com>
2024-06-17 21:21:51 -0400

    Time: 85 ms (53.69%), Space: 55.5 MB (76.57%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
@@ -6,13 +6,11 @@
 var findDifference = function(nums1, nums2) {
     let unique1=new Set(nums1);
     let unique2=new Set(nums2);
-
     for(item of unique1){
         if(unique2.has(item)){
             unique1.delete(item);
             unique2.delete(item);
         }
-    }
-    
+    }   
     return [[...unique1],[...unique2]]
 };
