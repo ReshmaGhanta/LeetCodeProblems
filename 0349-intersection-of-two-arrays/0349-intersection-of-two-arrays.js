93e45a67c3c05e76260d7a6ba7bedfb95048b389
Reshma Ghanta <45803247+ReshmaGhanta@users.noreply.github.com>
2024-06-17 21:34:29 -0400

    Time: 64 ms (18.47%), Space: 50.5 MB (57.82%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -7,10 +7,14 @@ var intersection = function(nums1, nums2) {
     let uniq1=new Set(nums1);
     let uniq2=new Set(nums2);
     let result=[];
-    for(item of uniq1){
-        if(uniq2.has(item)){
-            result.push(item)
+    
+    uniq1.forEach(item => {
+        if (uniq2.has(item)) {
+            result.push(item);
         }
-    }
+    });
     return result;
 };
+
+// T.C => O(N)
+// S.C => O(N+M)
